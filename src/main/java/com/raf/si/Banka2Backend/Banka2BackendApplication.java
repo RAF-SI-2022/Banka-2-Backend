28ddf375449a3559e34b0e056413a7b1ab95f8fa
ntadicrn4419 <ntadic4419rn@raf.rs>
2023-03-07 23:00:46 +0100

    init project structure

---
+++ src/main/java/com/raf/si/Banka2Backend/Banka2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.raf.si.Banka2Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Banka2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Banka2BackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/raf/si/Banka2Backend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.raf.si.Banka2Backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Banka2BackendApplication.class);
+	}
+
+}
