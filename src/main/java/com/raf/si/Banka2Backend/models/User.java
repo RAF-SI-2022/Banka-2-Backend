c13ffcbd59e196d6ffc6d9824122902107b9e207
Pavle Stevanovic <pstevanovic1219rn@raf.rs>
2023-03-10 01:27:56 +0100

    Dodata validacija

--- src/main/java/com/raf/si/Banka2Backend/models/User.java
+++ src/main/java/com/raf/si/Banka2Backend/models/User.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.List;
 
@@ -18,38 +19,44 @@ import java.util.List;
 @Entity
 @Table(
         name = "users",
-        uniqueConstraints = {@UniqueConstraint(columnNames = {"email", "jmbg"})})
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"email", "password", "jmbg"})})
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotNull(message="This field is required.")
     @Size(max = 50)
+    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$",
+            message = "Must enter a valid email.")
     @Email
     private String email;
 
-    @NotNull
+    @NotNull(message="This field is required.")
     @Size(min = 10)
+    @Pattern(regexp = "^.*(?=.{10,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
+            message = "Make sure the password has at least 10 characters, one digit, one lowercase and one uppercase letter and at least one special character.")
     private String password;
 
-    @NotNull
+    @NotNull(message="This field is required.")
     @Size(max = 20)
     private String firstName;
 
-    @NotNull
+    @NotNull(message="This field is required.")
     @Size(max = 20)
     private String lastName;
 
-    @NotNull
+    @NotNull(message="This field is required.")
     @Size(max = 13)
+    @Pattern(regexp = "^\\\\d+$",
+            message = "Invalid input.")
     private String jmbg;
 
-    @NotNull
+    @NotNull(message="This field is required.")
     @Size(max = 20)
     private String phone;
 
-    @NotNull
+    @NotNull(message="This field is required.")
     @Size(max = 20)
     private String jobPosition;
 
