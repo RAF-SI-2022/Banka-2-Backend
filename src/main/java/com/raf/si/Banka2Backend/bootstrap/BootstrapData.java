18ee15028659f8238994d4d1cc14d31254a4c6a8
Luka Jeremic <ljeremic4019rn@raf.rs>
2023-03-13 14:36:51 +0100

    bootstrap edit

--- src/main/java/com/raf/si/Banka2Backend/bootstrap/BootstrapData.java
+++ src/main/java/com/raf/si/Banka2Backend/bootstrap/BootstrapData.java
@@ -69,7 +69,7 @@ public class BootstrapData implements CommandLineRunner {
                 .active(ADMIN_ACTIVE)
                 .build();
 
-        // Add initial perms
+        // Add initial permissions
         List<Permission> permissions = new ArrayList<>();
         Permission adminPermission = new Permission(PermissionName.ADMIN_USER);
         Permission readPermission = new Permission(PermissionName.READ_USERS);
