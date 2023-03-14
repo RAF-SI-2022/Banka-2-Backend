42fa108893b8848deb12093fc2b18142d9d95894
Andreja Nesic <office@andrejanesic.com>
2023-03-14 01:44:09 +0100

    Fixed user regex patterns

--- src/main/java/com/raf/si/Banka2Backend/models/User.java
+++ src/main/java/com/raf/si/Banka2Backend/models/User.java
@@ -26,38 +26,37 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message="This field is required.")
-//    @Size(max = 50, message = "Input too long, cannot contain more than 50 characters.")
-//    @Pattern(regexp = "^(?=.{1,50}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$",
-//            message = "Must enter a valid email.")
-//    @Email
+    @NotNull(message = "This field is required.")
+    @Size(max = 50, message = "Input too long, cannot contain more than 50 characters.")
+    @Pattern(regexp = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+            message = "Must enter a valid email.")
+    @Email
     private String email;
 
     @JsonIgnore
-    @NotNull(message="This field is required.")
-    //@Pattern(regexp = "^.*(?=.{10,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$",
-    //        message = "Make sure the password has at least 10 characters, one digit, one lowercase and one uppercase letter and at least one special character.")
+    @NotNull(message = "This field is required.")
+    @Pattern(regexp = "^(?=.*?[a-zA-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,}$",
+            message = "Make sure the password has at least 10 characters, one digit, one lowercase and one uppercase letter and at least one special character.")
     private String password;
 
-    @NotNull(message="This field is required.")
+    @NotNull(message = "This field is required.")
     @Size(max = 20, message = "Input too long, cannot contain more than 20 characters.")
     private String firstName;
 
-    @NotNull(message="This field is required.")
+    @NotNull(message = "This field is required.")
     @Size(max = 20, message = "Input too long, cannot contain more than 20 characters.")
     private String lastName;
 
-    @NotNull(message="This field is required.")
+    @NotNull(message = "This field is required.")
     @Size(max = 13, message = "Input too long, cannot contain more than 13 characters.")
-//    @Pattern(regexp = "^\\\\d+$",
-//            message = "Invalid input.")
+//  @Pattern(regexp = "^\\d+$", message = "Only numbers are allowed in a JMBG identifier.")
     private String jmbg;
 
-    @NotNull(message="This field is required.")
+    @NotNull(message = "This field is required.")
     @Size(max = 20, message = "Input too long, cannot contain more than 20 characters.")
     private String phone;
 
-    @NotNull(message="This field is required.")
+    @NotNull(message = "This field is required.")
     @Size(max = 20, message = "Input too long, cannot contain more than 20 characters.")
     private String jobPosition;
 
