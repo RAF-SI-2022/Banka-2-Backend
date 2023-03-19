eca7e4eee48a9824a1f8877071042e699e486dc9
Luka Jeremic <ljeremic4019rn@raf.rs>
2023-03-19 17:27:33 +0100

    postavljen docker da radi

--- docker/healthcheck/dbexchange/healthcheck.sh
+++ docker/healthcheck/dbexchange/healthcheck.sh
@@ -7,5 +7,5 @@ pass="root"
 
 # Check availability & dbs
 mongosh "mongodb://localhost:27017" --username $user --password $pass --eval "use exchange-dev"
-mongosh "mongodb://localhost:27017" --username $user --password $pass --eval "use exchange-test"
-mongosh "mongodb://localhost:27017" --username $user --password $pass --eval "use exchange-prod"
+#mongosh "mongodb://localhost:27017" --username $user --password $pass --eval "use exchange-test"
+#mongosh "mongodb://localhost:27017" --username $user --password $pass --eval "use exchange-prod"
