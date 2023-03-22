fb3a06d5248452772a77b00b3d4d2ea3cb7710ac
Andreja Nesic <office@andrejanesic.com>
2023-03-22 11:41:49 +0100

    Removed old Maria models, added .gitkeeps

--- src/main/java/com/raf/si/Banka2Backend/repositories/mongodb/ForexRepository.java
+++
@@ -1,6 +0,0 @@
-package com.raf.si.Banka2Backend.repositories.mongodb;
-
-import com.raf.si.Banka2Backend.models.mongodb.Forex;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ForexRepository extends MongoRepository<Forex, String> {}
