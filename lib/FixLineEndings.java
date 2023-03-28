d42bf6746c3fb6f5ca6e9f11e2d229072b37ce19
Andreja Nesic <office@andrejanesic.com>
2023-03-28 10:36:08 +0200

    Added FixLineEndings code

--- lib/FixLineEndings.java
+++ lib/FixLineEndings.java
@@ -1,9 +1,34 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
- * Fixes line endings in passed files. #TODO complete
+ * Fixes line endings (CR/CRLF/LF > LF only) in critical files.
  */
 public class FixLineEndings {
 
-    public static void main(String[] args) {
+    private static final String SEP = File.separator;
 
+    public static void main(String[] args) {
+        String cwdir = System.getProperty("user.dir");
+        String[] files = new String[]{
+                "Makefile",
+                "mvnw",
+                "docker" + SEP + "test-devenv.sh",
+                "docker" + SEP + "healthcheck" + SEP + "mariadb" + SEP + "healthcheck.sh",
+                "docker" + SEP + "healthcheck" + SEP + "mongodb" + SEP + "healthcheck.sh"
+        };
+        for (String fname : files) {
+            try {
+                Path p = Paths.get(cwdir + SEP + fname);
+                String content = new String(Files.readAllBytes(p));
+                content = content.replaceAll("\\r\\n?", "\n");
+                Files.write(p, content.getBytes());
+                System.out.println("Wrote file: " + fname);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
