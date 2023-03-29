104eaef845084a7e0bd2334e7ad30037ee90bede
Luka Jeremic <ljeremic4019rn@raf.rs>
2023-03-29 12:27:02 +0200

    dodat forex i forex api (csv kao backup)

---
+++ src/main/java/com/raf/si/Banka2Backend/controllers/ForexController.java
@@ -0,0 +1,31 @@
+package com.raf.si.Banka2Backend.controllers;
+
+import com.raf.si.Banka2Backend.services.ForexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/forex")
+public class ForexController {
+
+  private final ForexService forexService;
+
+  @Autowired
+  public ForexController(ForexService forexService) {
+    this.forexService = forexService;
+  }
+
+  @GetMapping
+  public ResponseEntity<?> getAll() {
+    return ResponseEntity.ok().body(forexService.findAll());
+  }
+
+  @GetMapping("/{fromCurrency}/{toCurrency}")
+  public void test(
+      @PathVariable(name = "fromCurrency") String fromCurrency,
+      @PathVariable(name = "toCurrency") String toCurrency) {
+    System.out.println(forexService.getForexForCurrencies(fromCurrency, toCurrency));
+  }
+}
---
+++ src/main/java/com/raf/si/Banka2Backend/repositories/mariadb/ForexRepository.java
@@ -0,0 +1,13 @@
+package com.raf.si.Banka2Backend.repositories.mariadb;
+
+import com.raf.si.Banka2Backend.models.mariadb.Forex;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ForexRepository extends JpaRepository<Forex, Long> {
+
+  Optional<Forex> findForexByFromCurrencyCodeAndToCurrencyCode(
+      String fromCurrencyCode, String toCurrencyCode);
+}
