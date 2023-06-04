314289ac3e02752e3bb7ca2f39cc960ad56c44f7
DusanGligorijevic <dukacode@gmail.com>
2023-06-04 17:44:39 +0200

    - Saving pdf files to mongodb functionality updated.

--- otc/src/main/java/rs/edu/raf/si/bank2/otc/controllers/pdf/FileController.java
+++ otc/src/main/java/rs/edu/raf/si/bank2/otc/controllers/pdf/FileController.java
@@ -8,11 +8,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import rs.edu.raf.si.bank2.otc.models.mongodb.Contract;
+import rs.edu.raf.si.bank2.otc.models.mongodb.ContractElements;
+import rs.edu.raf.si.bank2.otc.models.mongodb.TransactionElement;
 import rs.edu.raf.si.bank2.otc.models.mongodb.pdf.LoadFile;
 import rs.edu.raf.si.bank2.otc.services.pdf.FileService;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -24,6 +29,13 @@ public class FileController {
 
     @PostMapping("/upload")
     public ResponseEntity<?> upload(@RequestParam("file")MultipartFile file) throws IOException {
+        ContractElements contractElements1 = ContractElements.FINALISED;
+        TransactionElement transactionElement = new TransactionElement("1234",ContractElements.SELL,"STOCK",ContractElements.FINALISED,"USD",45.5,12.2);
+        TransactionElement transactionElement2 = new TransactionElement("1234",ContractElements.SELL,"STOCK",ContractElements.FINALISED,"USD",45.5,12.2);
+        TransactionElement transactionElement3 = new TransactionElement("1234",ContractElements.SELL,"STOCK",ContractElements.FINALISED,"USD",45.5,12.2);
+        Contract contract = new Contract("123", contractElements1,"creation","last","number","desc", List.of(transactionElement, transactionElement2, transactionElement3));
+        File f = fileService.createContractFile(contract);
+        fileService.saveFile(f);
         return new ResponseEntity<>(fileService.addFile(file), HttpStatus.OK);
     }
 
