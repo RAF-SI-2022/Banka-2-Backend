7d92aa27a17395c5729b9ecd7b1bbf9b1932b210
Andreja Nesic <office@andrejanesic.com>
2023-06-08 19:12:00 +0200

    Update provisioning command

--- kubernetes/provision/mongodb/provision.sh
+++ kubernetes/provision/mongodb/provision.sh
@@ -18,9 +18,22 @@ export MONGODB_ROOT_PASSWORD=$(kubectl get secret --namespace "${NAMESPACE}" mon
 
 echo "Creating monogo user ${NEW_MONGODB_USER} for database ${NEW_MONGODB_DB}..."
 
-kubectl --namespace="${NAMESPACE}" exec $(kubectl --namespace="${NAMESPACE}" get pods -o name | grep -m1 mongodb-sharded-mongos | cut -d'/' -f 2) -c mongos -- mongosh -u "root" -p "${MONGODB_ROOT_PASSWORD}" --authenticationDatabase admin --authenticationMechanism=SCRAM-SHA-256 --eval 'db.getSiblingDB("admin").createRole({ role: "SystemRole", privileges: [{resource:{db:"admin",collection:""},actions:["killop","inprog"]}, {resource:{db:"'"${NEW_MONGODB_DB}"'",collection:""},actions:["killop","inprog"]}], roles: [] });'
-kubectl --namespace="${NAMESPACE}" exec $(kubectl --namespace="${NAMESPACE}" get pods -o name | grep -m1 mongodb-sharded-mongos | cut -d'/' -f 2) -- mongosh -u "root" -p "${MONGODB_ROOT_PASSWORD}" --authenticationDatabase admin --authenticationMechanism=SCRAM-SHA-256 --eval 'db.getSiblingDB("admin").createUser({user:"'"${NEW_MONGODB_USER}"'",pwd:"'"${NEW_MONGODB_USER_PASSWORD}"'",roles:[{role:"readWrite",db:"'"${NEW_MONGODB_DB}"'"},{role:"SystemRole",db:"admin"},"clusterManager"]});'
+until kubectl --namespace="${NAMESPACE}" exec $(kubectl --namespace="${NAMESPACE}" get pods -o name | grep -m1 mongodb-sharded-mongos | cut -d'/' -f 2) -c mongos -- mongosh -u "root" -p "${MONGODB_ROOT_PASSWORD}" --authenticationDatabase admin --authenticationMechanism=SCRAM-SHA-256 --eval 'db.getSiblingDB("admin").createRole({ role: "SystemRole", privileges: [{resource:{db:"admin",collection:""},actions:["killop","inprog"]}, {resource:{db:"'"${NEW_MONGODB_DB}"'",collection:""},actions:["killop","inprog"]}], roles: [] });'
+do
+  echo "Error, trying again in 30s..."
+  sleep 30
+done
+
+until kubectl --namespace="${NAMESPACE}" exec $(kubectl --namespace="${NAMESPACE}" get pods -o name | grep -m1 mongodb-sharded-mongos | cut -d'/' -f 2) -- mongosh -u "root" -p "${MONGODB_ROOT_PASSWORD}" --authenticationDatabase admin --authenticationMechanism=SCRAM-SHA-256 --eval 'db.getSiblingDB("admin").createUser({user:"'"${NEW_MONGODB_USER}"'",pwd:"'"${NEW_MONGODB_USER_PASSWORD}"'",roles:[{role:"readWrite",db:"'"${NEW_MONGODB_DB}"'"},{role:"SystemRole",db:"admin"},"clusterManager"]});'
+do
+  echo "Error, trying again in 30s..."
+  sleep 30
+done
 
 echo "Enabling sharding for database ${NEW_MONGODB_DB}"
 
-kubectl --namespace="${NAMESPACE}" exec $(kubectl --namespace="${NAMESPACE}" get pods -o name | grep -m1 mongodb-sharded-mongos | cut -d'/' -f 2) -c mongos -- mongosh -u "root" -p "${MONGODB_ROOT_PASSWORD}" --authenticationDatabase admin --authenticationMechanism=SCRAM-SHA-256 --eval "sh.enableSharding('${NEW_MONGODB_DB}');"
+until kubectl --namespace="${NAMESPACE}" exec $(kubectl --namespace="${NAMESPACE}" get pods -o name | grep -m1 mongodb-sharded-mongos | cut -d'/' -f 2) -c mongos -- mongosh -u "root" -p "${MONGODB_ROOT_PASSWORD}" --authenticationDatabase admin --authenticationMechanism=SCRAM-SHA-256 --eval "sh.enableSharding('${NEW_MONGODB_DB}');"
+do
+  echo "Error, trying again in 30s..."
+  sleep 30
+done
