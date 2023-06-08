fd54262d48af8c998a5027fe576a81a26c4e56e4
Andreja Nesic <office@andrejanesic.com>
2023-06-08 19:30:08 +0200

    Typo fix

--- kubernetes/provision/mongodb/remove.sh
+++ kubernetes/provision/mongodb/remove.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 helm uninstall mongodb
-kubectl pvc delete datadir-mongodb-mongodb-sharded-configsvr-0
-kubectl pvc delete datadir-mongodb-mongodb-sharded-shard0-data-0
-kubectl pvc delete datadir-mongodb-mongodb-sharded-shard1-data-0
+kubectl delete pvc datadir-mongodb-mongodb-sharded-configsvr-0
+kubectl delete pvc datadir-mongodb-mongodb-sharded-shard0-data-0
+kubectl delete pvc datadir-mongodb-mongodb-sharded-shard1-data-0
