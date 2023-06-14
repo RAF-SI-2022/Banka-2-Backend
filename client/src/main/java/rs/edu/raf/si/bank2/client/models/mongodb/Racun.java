5fcbb4355ca58786dd5066cd72332094dd220613
Luka Jeremic <ljeremic4019rn@raf.rs>
2023-06-14 13:56:13 +0200

    dodato gettovanje balansa za klijenta

---
+++ client/src/main/java/rs/edu/raf/si/bank2/client/models/mongodb/Racun.java
@@ -0,0 +1,10 @@
+package rs.edu.raf.si.bank2.client.models.mongodb;
+
+import rs.edu.raf.si.bank2.client.models.mongodb.enums.Balance;
+import rs.edu.raf.si.bank2.client.models.mongodb.enums.BalanceStatus;
+import rs.edu.raf.si.bank2.client.models.mongodb.enums.BalanceType;
+
+
+public abstract class Racun {
+
+}
